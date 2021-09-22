22dd12dd688c8d08013437f6a16b837b2a9c2dc3
Sergej V <vasiliokas@gmail.com>
2021-09-22 23:56:02 +0100

    init iosApprentice

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+            VStack {
+                Text("Welcome to me app !<br> another line")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.green)
+                .padding()
+
+
+                
+            }
+            Button(action: {
+                print("test")
+                self.alertIsVisible = true
+            }) {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert( title: Text("Title Popup"),
+                message: Text("Sub-title: this is my 1 popup"),
+                       dismissButton: .default(Text("Awsesome")))
+            }
+        }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
