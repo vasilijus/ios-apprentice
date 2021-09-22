c2e16cb06d2b28d763fa5cc03b4b8c3c707dbff3
Sergej V <vasiliokas@gmail.com>
2021-09-22 23:05:04 +0100

    Initial Commit

---
+++ Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Sergej on 22/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
